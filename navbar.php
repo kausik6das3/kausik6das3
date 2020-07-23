1925f33faf92d60fe19f9895c5d0be4bc6d79530
Kausik Das <kausik6das3@gmail.com>
2020-07-23 11:20:44 +0530

    Internal Commit

---
+++ navbar.php
@@ -0,0 +1,7 @@
+<div class="navbar" id="myNavbar">
+  	<a href="index.htm"class="active">Home</a>
+  	<a href="linux.htm">Linux</a>
+  	<a href="photoalbum.htm">Photoalbum</a>
+  	<a href="javascript:void(-1);" style="font-size:15px;" class="icon" onclick="myFunction()">&#9776;</a>
+</div>
+
