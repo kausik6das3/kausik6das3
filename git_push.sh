b92d2ea251debc42383355e5a939fb38ab65a3be
kausik6das3 <kausik6das3@gmail.com>
2020-07-29 12:03:13 +0530

    Internal Commit

--- git_push.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash 
-cp -r * /home/kausik/Public/kausik6das3.github.io
-cd /home/kausik/Public/kausik6das3.github.io
-git add --all
-git config --global user.email "kausik6das3@gmail.com"
-git config --global user.name "kausik6das3"
-git commit -m "Internal Commit"
-git push -u origin master
-
